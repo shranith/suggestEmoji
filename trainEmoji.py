8ff9d87b80f3adbbda03a879aaa1ccb02b826c67
Shranith Linga <shranith.linga1@ibm.com>
2018-05-21 17:48:38 -0600

    ADD: Training and test datasets and version v1

--- trainEmoji.py
+++ trainEmoji.py
@@ -1,23 +1,147 @@
+# _*_ coding:utf-8 _*_
 import numpy as np
-import tensorflow as tf
+from utils import *
 import emoji
-
-def load_datasets():
-    f = open('data/X_train.txt','r')
-    X_train = np.asarray(f.readlines())
-
-    f = open('data/Y_train.txt','r')
-    Y_train = np.asarray(f.readlines())
-
-    f = open('data/X_test.txt','r')
-    X_test = np.asarray(f.readlines())
-
-    f = open('data/Y_test.txt','r')
-    Y_test = np.asarray(f.readlines())
-
-    return X_train, Y_train, X_test, Y_test
+import matplotlib.pyplot as plt
 
 
+def sentences_to_indices(X, word_to_index, max_len):
+    m = X.shape[0]
+    X_indices = np.zeros((m, max_len))
+    for i in range(m):
+        sentence_words = X[i].lower().split()
+        j = 0
+        for w in sentence_words:
+            X_indices[i, j] = word_to_index[w]
+            j = j + 1
+    return X_indices
+
+def pretrained_embedding_layer(word_to_vec_map, word_to_index):
+    vocab_len = len(word_to_index) + 1  #word index begin with 1,plus 1 for padding 0
+    emb_dim = word_to_vec_map["cucumber"].shape[0]
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+    for word, index in word_to_index.items():
+        emb_matrix[index, :] = word_to_vec_map[word]
+    embedding_layer = Embedding(vocab_len, emb_dim, trainable=False)
+    embedding_layer.build((None,))
+    embedding_layer.set_weights([emb_matrix])
+    return embedding_layer
+
+def predict(X, Y, W, b, word_to_vec_map):
+    m = Y.shape[0]                          # number of training examples
+    a = []
+    for i in range(m):                                # Loop over the training examples
+
+        ### START CODE HERE ### (≈ 4 lines of code)
+        # Average the word vectors of the words from the i'th training example
+        avg = sentence_to_avg(X[i], word_to_vec_map)
+
+        # Forward propagate the avg through the softmax layer
+        z = np.dot(W, avg) + b
+        l = list(softmax(z))
+        max_value = max(l)
+        a.append(l.index(max_value))
+    return np.asarray(a)
+
+def sentence_to_avg(sentence, word_to_vec_map):
+    """
+    Converts a sentence (string) into a list of words (strings). Extracts the GloVe representation of each word
+    and averages its value into a single vector encoding the meaning of the sentence.
+
+    Arguments:
+    sentence -- string, one training example from X
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+
+    Returns:
+    avg -- average vector encoding information about the sentence, numpy-array of shape (50,)
+    """
+
+    ### START CODE HERE ###
+    # Step 1: Split sentence into list of lower case words (≈ 1 line)
+    words = sentence.lower().split()
+
+    # Initialize the average word vector, should have the same shape as your word vectors.
+    avg = np.zeros(word_to_vec_map[words[0]].shape)
+
+    # Step 2: average the word vectors. You can loop over the words in the list "words".
+    for w in words:
+        avg += word_to_vec_map[w]
+    avg = avg/len(words)
+
+    ### END CODE HERE ###
+
+    return avg
+
+def Emojify_V1(X, Y, word_to_vec_map, learning_rate = 0.01, num_iterations = 400):
+    """
+    Model to train word vector representations in numpy.
+
+    Arguments:
+    X -- input data, numpy array of sentences as strings, of shape (m, 1)
+    Y -- labels, numpy array of integers between 0 and 7, numpy-array of shape (m, 1)
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+    learning_rate -- learning_rate for the stochastic gradient descent algorithm
+    num_iterations -- number of iterations
+
+    Returns:
+    pred -- vector of predictions, numpy-array of shape (m, 1)
+    W -- weight matrix of the softmax layer, of shape (n_y, n_h)
+    b -- bias of the softmax layer, of shape (n_y,)
+    """
+
+    np.random.seed(1)
+
+    # Define number of training examples
+    m = Y.shape[0]                          # number of training examples
+    n_y = 5                                 # number of classes
+    n_h = 50                                # dimensions of the GloVe vectors
+
+    # Initialize parameters using Xavier initialization
+    W = np.random.randn(n_y, n_h) / np.sqrt(n_h)
+    b = np.zeros((n_y,))
+
+    # Convert Y to Y_onehot with n_y classes
+    Y_oh = convert_to_one_hot(Y, C = n_y)
+
+    # Optimization loop
+    for t in range(num_iterations):                       # Loop over the number of iterations
+        for i in range(m):                                # Loop over the training examples
+
+            ### START CODE HERE ### (≈ 4 lines of code)
+            # Average the word vectors of the words from the i'th training example
+            avg = sentence_to_avg(X[i], word_to_vec_map)
+
+            # Forward propagate the avg through the softmax layer
+            z = np.dot(W, avg) + b
+            a = softmax(z)
+
+            # Compute cost using the i'th training label's one hot representation and "A" (the output of the softmax)
+            cost = -1*np.sum(np.multiply(Y_oh[i],np.log(a)))
+            ### END CODE HERE ###
+
+            # Compute gradients
+            dz = a - Y_oh[i]
+            dW = np.dot(dz.reshape(n_y,1), avg.reshape(1, n_h))
+            db = dz
+
+            # Update parameters with Stochastic Gradient Descent
+            W = W - learning_rate * dW
+            b = b - learning_rate * db
+
+        if t % 100 == 0:
+            print("Epoch: " + str(t) + " --- cost = " + str(cost))
+            pred = predict(X, Y, W, b, word_to_vec_map)
+
+    return pred, W, b
 
 if __name__ == '__main__':
-    print(load_datasets())
+
+    X_train, Y_train = read_csv('data/dataTrain.csv')
+    X_test, Y_test = read_csv('data/dataTest.csv')
+    maxLen = len(max(X_train, key=len).split())
+    word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+    pred, W, b = Emojify_V1(X_train, Y_train, word_to_vec_map)
+
+    print(pred)
+    print(Y_train)
